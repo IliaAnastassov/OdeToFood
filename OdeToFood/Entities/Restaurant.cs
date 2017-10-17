d3f4a149586e48e256651c9115327a410856b543
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-10-17 15:22:14 +0300

    Add Create view. Update Restaurant entity and Home controller

--- OdeToFood/Entities/Restaurant.cs
+++ OdeToFood/Entities/Restaurant.cs
@@ -1,9 +1,20 @@
 ﻿namespace OdeToFood.Entities
 {
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        Franch,
+        Japanese,
+        American
+    }
+
     public class Restaurant
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public CuisineType Cuisine { get; set; }
     }
 }
