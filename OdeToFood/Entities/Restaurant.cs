f2436f795d99ce3b6bb9275106a67167f70eee68
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-10-19 23:40:59 +0300

    Update Restaurant entity

--- OdeToFood/Entities/Restaurant.cs
+++ OdeToFood/Entities/Restaurant.cs
@@ -1,4 +1,6 @@
-﻿namespace OdeToFood.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Entities
 {
     public enum CuisineType
     {
@@ -13,6 +15,9 @@
     {
         public int Id { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a name for the restaurant")]
+        [MaxLength(80)]
+        [Display(Name = "Restaurant Name")]
         public string Name { get; set; }
 
         public CuisineType Cuisine { get; set; }
